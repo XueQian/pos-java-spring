4c6ca717297f5ac2148683ff7830eb7a1e28871d
liyanzi <843624084@qq.com>
2014-11-05 19:40:29 +0800

    modify getItem method in ItemDaoImpl

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/impl/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/impl/ItemDaoImpl.java
@@ -6,37 +6,42 @@ import com.thoughtworks.iamcoach.pos.entity.Item;
 import com.thoughtworks.iamcoach.pos.entity.Promotion;
 import com.thoughtworks.iamcoach.pos.entity.PromotionFactory;
 import com.thoughtworks.iamcoach.pos.util.JdbcUtil;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDaoImpl implements ItemDao {
-    ResultSet rs;
-    PreparedStatement pre;
     private JdbcUtil jdbcUtil = new JdbcUtil();
+    private JdbcTemplate jdbcTemplate;
+    private  ResultSet rs;
+    private  PreparedStatement pre;
+
+    public ItemDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
-    public Item getItem(String barcode) {
-        Connection conn = jdbcUtil.getConnection();
-        Item item = null;
-        try {
-            pre = conn.prepareStatement("select * from items i,categories c where  i.i_categoryid = c.c_id and i_barcode =?");
-            pre.setString(1, barcode);
-            rs = pre.executeQuery();
-            rs.next();
-            item = new Item(rs.getInt("i_id"), barcode, rs.getString("i_name"), rs.getString("i_unit"), rs.getDouble("i_price"), rs.getString("c_name"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                rs.close();
-                pre.close();
-                jdbcUtil.closeConnection();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
+    public Item getItem(final String barcode) {
+
+        final Item item = new Item();
+        jdbcTemplate.query("select * from items i,categories c where  i.i_categoryid = c.c_id and i_barcode = ?",
+                new Object[] {barcode},
+                new RowCallbackHandler() {
+                    public void processRow(ResultSet rs) throws SQLException {
+                        item.setId(rs.getInt("i_id"));
+                        item.setBarcode(barcode);
+                        item.setName(rs.getString("i_name"));
+                        item.setUnit(rs.getString("i_unit"));
+                        item.setPrice(rs.getDouble("i_price"));
+                        item.setCategory(rs.getString("c_name"));
+                    }
+                });
         return item;
     }
 
@@ -79,7 +84,6 @@ public class ItemDaoImpl implements ItemDao {
         }
         return itemPromotions;
     }
-
 }
 
 
