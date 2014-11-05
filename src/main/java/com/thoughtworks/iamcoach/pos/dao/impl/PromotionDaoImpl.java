dfaca38a5663abcc2e9b26383b2c1d4e6a19e1d3
liyanzi <843624084@qq.com>
2014-11-05 21:29:03 +0800

    modify getPromotionBarcode method in PromotionDaoImpl

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/impl/PromotionDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/impl/PromotionDaoImpl.java
@@ -3,21 +3,15 @@ package com.thoughtworks.iamcoach.pos.dao.impl;
 import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
 import com.thoughtworks.iamcoach.pos.entity.Promotion;
 import com.thoughtworks.iamcoach.pos.entity.PromotionFactory;
-import com.thoughtworks.iamcoach.pos.util.JdbcUtil;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class PromotionDaoImpl implements PromotionDao {
-    ResultSet rs;
-    PreparedStatement pre;
-    private JdbcUtil jdbcUtil = new JdbcUtil();
 
     private JdbcTemplate jdbcTemplate;
 
@@ -46,27 +40,15 @@ public class PromotionDaoImpl implements PromotionDao {
 
     @Override
     public Set<String> getPromotionBarcode() {
-        Connection conn = jdbcUtil.getConnection();
-        Set<String> promotionBarcodes = new HashSet<String>();
-        try {
-            pre = conn.prepareStatement("select * from items i, items_promotions ip where i.i_id = ip.itemid ");
-            rs = pre.executeQuery();
-            while (rs.next()) {
-                promotionBarcodes.add(rs.getString("i_barcode"));
-            }
+        final Set<String> promotionBarcodes = new HashSet<String>();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                rs.close();
-                pre.close();
-                jdbcUtil.closeConnection();
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            }
-        }
+        jdbcTemplate.query("select * from items i, items_promotions ip where i.i_id = ip.itemid ",
+                new Object[]{},
+                new RowCallbackHandler() {
+                    public void processRow(ResultSet rs) throws SQLException {
+                        promotionBarcodes.add(rs.getString("i_barcode"));
+                    }
+                });
         return promotionBarcodes;
     }
-
 }
