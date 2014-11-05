1721716af6dc1e00f90127a76639437a49cbdaf8
liyanzi <843624084@qq.com>
2014-11-05 19:40:58 +0800

    add set method

--- src/main/java/com/thoughtworks/iamcoach/pos/entity/Item.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/entity/Item.java
@@ -24,6 +24,30 @@ public class Item {
         this.category = category;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public String getUnit() {
         return unit;
     }
