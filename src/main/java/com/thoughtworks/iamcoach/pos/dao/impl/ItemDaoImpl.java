0a56b9677311b82898fb64fd058306ea56e1fb4f
Xue Qian <xueqian0321@126.com>
2014-11-06 20:26:17 +0800

    modify spring getItemPromotions --another implement

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/impl/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/impl/ItemDaoImpl.java
@@ -8,12 +8,10 @@ import com.thoughtworks.iamcoach.pos.entity.PromotionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDaoImpl implements ItemDao {
@@ -46,39 +44,63 @@ public class ItemDaoImpl implements ItemDao {
     public Item getItem(String barcode) {
         String sql = "select * from items i,categories c where  i.i_categoryid = c.c_id and i_barcode = ?";
 
-        return jdbcTemplate.queryForObject(sql,new Object[]{barcode},new RowMapper<Item>(){
+        return jdbcTemplate.queryForObject(sql, new Object[]{barcode}, new RowMapper<Item>() {
             @Override
             public Item mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return new Item(rs.getInt("i_id"),rs.getString("i_barcode"),rs.getString("i_Name"),rs.getString("i_unit"),rs.getDouble("i_price"),rs.getString("c_name"));
+                return new Item(rs.getInt("i_id"), rs.getString("i_barcode"), rs.getString("i_Name"), rs.getString("i_unit"), rs.getDouble("i_price"), rs.getString("c_name"));
             }
         });
     }
 
     @Override
     public List<Promotion> getItemPromotions(String barcode) {
+        String sql = "select * from items i,items_promotions ip where i.i_id=ip.itemid  and i_barcode = ?";
 
-        final List<Promotion> itemPromotions = new ArrayList<Promotion>();
+        return jdbcTemplate.query(sql, new Object[]{barcode}, new RowMapper<Promotion>() {
+            @Override
+            public Promotion mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-        jdbcTemplate.query("select * from items i,items_promotions ip where i.i_id=ip.itemid  and i_barcode = ?",
-                new Object[]{barcode},
-                new RowCallbackHandler() {
-                    public void processRow(ResultSet rs) throws SQLException {
-                        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
-                        PromotionDao promotionDaoImpl = (PromotionDao) applicationContext.getBean("promotionDaoImpl");
+                ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+                PromotionDao promotionDaoImpl = (PromotionDao) applicationContext.getBean("promotionDaoImpl");
 
-                        int promotionId = rs.getInt("promotionid");
-                        Promotion promotionForType = promotionDaoImpl.getPromotion(promotionId);
+                int promotionId = rs.getInt("promotionid");
+                Promotion promotionForType = promotionDaoImpl.getPromotion(promotionId);
 
-                        Promotion promotion = PromotionFactory.getPromotionByType(promotionForType.getType());
-                        promotion.setId(promotionForType.getId());
-                        promotion.setType(promotionForType.getType());
-                        promotion.setDescription(promotionForType.getDescription());
-                        promotion.setDiscount(rs.getDouble("discount"));
-                        itemPromotions.add(promotion);
-                    }
-                });
-        return itemPromotions;
+                Promotion promotion = PromotionFactory.getPromotionByType(promotionForType.getType());
+                promotion.setId(promotionForType.getId());
+                promotion.setType(promotionForType.getType());
+                promotion.setDescription(promotionForType.getDescription());
+                promotion.setDiscount(rs.getDouble("discount"));
+                return promotion;
+            }
+        });
     }
+// getItemPromotions　spring　另一种实现方法
+//    @Override
+//    public List<Promotion> getItemPromotions(String barcode) {
+//
+//        final List<Promotion> itemPromotions = new ArrayList<Promotion>();
+//
+//        jdbcTemplate.query("select * from items i,items_promotions ip where i.i_id=ip.itemid  and i_barcode = ?",
+//                new Object[]{barcode},
+//                new RowCallbackHandler() {
+//                    public void processRow(ResultSet rs) throws SQLException {
+//                        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+//                        PromotionDao promotionDaoImpl = (PromotionDao) applicationContext.getBean("promotionDaoImpl");
+//
+//                        int promotionId = rs.getInt("promotionid");
+//                        Promotion promotionForType = promotionDaoImpl.getPromotion(promotionId);
+//
+//                        Promotion promotion = PromotionFactory.getPromotionByType(promotionForType.getType());
+//                        promotion.setId(promotionForType.getId());
+//                        promotion.setType(promotionForType.getType());
+//                        promotion.setDescription(promotionForType.getDescription());
+//                        promotion.setDiscount(rs.getDouble("discount"));
+//                        itemPromotions.add(promotion);
+//                    }
+//                });
+//        return itemPromotions;
+//    }
 
 }
 
